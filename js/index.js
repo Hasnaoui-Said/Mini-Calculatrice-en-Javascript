575ac849ed8dcecabab50f8a78515590193217ba
hasnaoui-said <saidhasnaoui.uce@gmail.com>
2022-03-14 08:55:55 +0100

    Add function and translate cal simple

--- js/index.js
+++ js/index.js
@@ -1,99 +1,106 @@
-// DOM
-const btns = [...document.querySelectorAll('.btn'), document.querySelector('.tag-x')];
-const listCode = btns.map(e=>e.dataset.code);
-const operation = document.querySelector('.operation');
-const resultat = document.querySelector('.resultat');
-// la fonction map prend chaque element de tab et faire qlqc puis return le resultat en un nouveau tab
-// const tab = [0, 1, 2, 3, 4]
-// const nvTab = tab.map(e =>e.toString()+" toString");
-// console.log(tab);
-// console.log(nvTab);
-
-document.addEventListener('keydown', e => {
-    const value = e.keyCode.toString();
-    faireCalcule(value);
-});
-document.addEventListener('click', e => {
-    const value = e.target.dataset.code;
-    faireCalcule(value);
-});
-
-const faireCalcule = value => {
-    if(listCode.includes(value)){
-        switch(value){
-            case '13':
-                const val = eval(operation.textContent);
-                operation.textContent = val;
-                resultat.textContent = "";
+let ecran = document.querySelector('.operationOpp')
+var myResult = "0", myNumber = "", myOperateur = "";
+let virgule = document.querySelector('.point')
+let virguleDown = false
+let negatif = document.querySelector('.btn_plus_moins')
+let negatifDown = false
+let operations = document.querySelectorAll('.operation')
+const operate = (myOperateur,a,b) => {
+    let result = '';
+    switch (myOperateur) {
+        case '+':
+            result = additionner(a,b);
             break;
-            case '8':
-                let str = operation.textContent;
-                let newStr = str.substring(0, str.length - 1);
-                operation.textContent = newStr;
-                resultat.textContent = "";
-                break;
-            case '46':
-                operation.textContent = "";
-                resultat.textContent = "";
-                break;
-            case '9897':
-                operation.textContent += "+(-";
-                break;
-            case '61':
-                operation.textContent += "+";
-                break;
-            case '9899':
-                operation.textContent += "-";
-                break;
-            case '170':
-                operation.textContent += "*";
-                break;
-            case '58':
-                operation.textContent += "/";
-                break;
-            case '169':
-                operation.textContent += ")";
-                break;
-            case '9898':
-                operation.textContent += "(";
-                break;
-            case '59':
-                if(operation.textContent[operation.textContent.length-1] == '.'){
-                    break;
-                }
-                if(operation.textContent == '' || 
-                        operation.textContent[operation.textContent.length-1] == '+' || 
-                        operation.textContent[operation.textContent.length-1] == '-' || 
-                        operation.textContent[operation.textContent.length-1] == '/' || 
-                        operation.textContent[operation.textContent.length-1] == '*' ){
-                    operation.textContent += "0.";
-                    break;
-                }
-                let strP = lastChaine(operation.textContent,'+');
-                let strM = lastChaine(operation.textContent,'-');
-                let strF = lastChaine(operation.textContent,'*');
-                let strD = lastChaine(operation.textContent,'/');
-                
-                if(
-                    strP.includes('.') || strM.includes('.') ||strF.includes('.') ||strD.includes('.')){
-                    break;
-                }
-
-                operation.textContent += ".";
-                break;
-            default:
-                let indexCode = listCode.indexOf(value);
-                let btn = btns[indexCode];
-                operation.textContent += btn.innerHTML;
-                // 4
-                const myRes = eval(operation.textContent);
-                resultat.textContent = myRes;
-                
+            case '-':
+            result = soustraire(a,b);
+            break;
+        case '*':
+            result = multiplier(a,b);
+            break;
+        case '/':
+            result = diviser(a,b);
+            break;
+        default:
+            result = String(a);
         }
-    }
+        return result;
 }
-let lastChaine = (str, smb) => str.substr(str.lastIndexOf(smb));
-
-window.addEventListener('error', e => {
-    alert("erreur dans votre calcule: "+ e.message);
-});
+operations.forEach((operation) => {
+    operation.addEventListener('click',() => {
+        if(myNumber === ""){
+            myOperateur = operation.id;
+        }else{
+            myResult = operate(myOperateur,parseFloat(myResult),parseFloat(myNumber));
+            myOperateur = operation.id;
+            myNumber = "";
+        }
+        virguleDown = false;
+        negatifDown = false;
+    ecran.innerHTML = myResult + myOperateur + myNumber;
+    })
+})
+let egale = document.querySelector('.egal');
+egale.addEventListener('click',() => {
+    myResult = operate(myOperateur,parseFloat(myResult),parseFloat(myNumber));
+    myNumber = "";
+    myOperateur = "";
+    virguleDown = true;
+    negatifDown = true;
+    ecran.innerHTML = myResult + myOperateur + myNumber;
+})
+ecran.innerHTML = myResult + myOperateur + myNumber;
+let clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    myResult = "0";
+    myNumber = "";
+    myOperateur = "";
+    virguleDown = false;
+    negatifDown = false;
+    ecran.innerHTML = myResult + myOperateur + myNumber;
+})
+let btnNumbers = document.querySelectorAll('.number');
+btnNumbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        if(myOperateur === ""){
+            if(myResult === "0")
+                myResult = number.innerHTML;
+            else
+            myResult += number.innerHTML;
+        }
+        else{
+            if(myNumber === "0")
+            myNumber = number.innerHTML;
+            else
+            myNumber += number.innerHTML;
+        }
+        ecran.innerHTML = myResult + myOperateur + myNumber;
+    })
+})
+virgule.addEventListener('click',() => {
+    if(!virguleDown){
+        if(myOperateur === "")
+            myResult += ".";
+            else
+                myNumber += ".";
+            virguleDown = true;
+            ecran.innerHTML = myResult + myOperateur + myNumber;
+        }
+    })
+    negatif.addEventListener('click',() => {
+        if(!negatifDown){
+            if(myOperateur === "")
+                myResult = '-'+myResult;
+            else
+                myNumber = '-'+myNumber;
+            negatifDown = true;
+            ecran.innerHTML = myResult + myOperateur + myNumber;
+        }
+})
+const additionner = (num1, num2) => num1 + num2
+const soustraire = (num1, num2) => num1 - num2
+const multiplier = (num1, num2) => num1 * num2
+const diviser = (num1, num2) => {
+    if(num2 == 0)
+    return null;
+    return num1 / num2;
+}
